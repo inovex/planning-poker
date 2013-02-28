4626a5c48f9b0f240cb8cbaa5dd217b02df85799
Dominik Jungowski <Dominik@Jungowski.de>
2013-02-28 17:58:54 +0100

    userlogin, first draft

--- poker-server.js
+++ poker-server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 var express = require('express');
 
 var app = express();
@@ -26,9 +27,31 @@ app.get('/', function(req, res) {
 	res.end();
 });
 
-app.get('/login', function(req, res) {
-	currentUsers;
-	res.end();
+app.post('/login', function(req, res) {
+    var user;
+    user = '';
+    //res.set('Content-Type', 'application/json');
+    req.on('data', function (data) {
+        user += data;
+        if (user.length > 1e6) {
+            // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+            request.connection.destroy();
+        }
+    });
+    req.on('end', function () {
+        user = qs.parse(user);
+        // Create random id for user
+        user.id = 0;
+        //console.log(user);
+        console.log(new Date());
+        currentUsers[user.id] = user;
+        console.log(currentUsers);
+
+        res.json(user);
+        //res.write(JSON.stringify(user));
+    });
+
+	//res.end();
 });
 
 app.listen(1337, '127.0.0.1');
--- public/js/jquery/modules/jquery.login.js
+++ public/js/jquery/modules/jquery.login.js
@@ -1,5 +1,38 @@
 (function(jQuery, $) {
 	jQuery.fn.login = function(options) {
-		console.log(localStorage);
+		options = jQuery.extend({}, jQuery.fn.login.options, options);
+		var user;
+
+		user = localStorage.getItem('user');
+		if (user !== null) {
+			$(options.overlay).hide();
+		}
+
+		// Klickhandler registrieren
+		$(this).each(function(key, item) {
+			var eventData;
+
+			eventData = {
+				options: options,
+				form: item
+			};
+
+			$(item).on('submit', eventData, jQuery.fn.login.loginUser);
+		});
+	};
+
+	jQuery.fn.login.loginUser = function(event) {
+		var loadOptions;
+
+		event.preventDefault();
+		loadOptions = {
+			method: 'POST',
+			data: $(event.data.form).serialize()
+		};
+		$.ajax('/login', loadOptions);
+	};
+
+	jQuery.fn.login.options = {
+		overlay: null
 	};
 })(jQuery, jQuery);
