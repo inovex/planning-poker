f05e3aba15a6bf4f226327f6a2312bc31c5a64e2
Dominik Jungowski <Dominik@Jungowski.de>
2013-06-10 13:19:42 +0200

    added ;

--- poker-users.js
+++ poker-users.js
@@ -3,7 +3,7 @@ module.exports.currentUsers = {};
 module.exports.remove = function(userId) {
 	this.currentUsers[userId] = null;
     delete this.currentUsers[userId];
-}
+};
 
 module.exports.add = function(user) {
 	this.currentUsers[user.id] = user;
