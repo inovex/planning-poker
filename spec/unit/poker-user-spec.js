f6ae6d51648d69123e8559e915dcc22e3dd26f44
Dominik Jungowski <Dominik@Jungowski.de>
2013-12-25 13:59:35 +0100

    new Poker User class which creates a new user + spec

--- spec/unit/poker-user-spec.js
+++ spec/unit/poker-user-spec.js
@@ -1,7 +1,12 @@
 var user = require('../../lib/poker-user.js');
 
 describe('User', function() {
-    it('should create a new user', function() {
+    it('should create a new user', function(done) {
+        user.create();
 
+        user.on('user-created', function(newUser) {
+            expect(newUser.id.length).toBe(40);
+            done();
+        });
     });
 });
