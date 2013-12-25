1b9577e43fce4200f4dc8e72c4694c7cc1e7ac58
Dominik Jungowski <Dominik@Jungowski.de>
2013-12-25 14:33:56 +0100

    only create new users with user details

--- spec/unit/poker-user-spec.js
+++ spec/unit/poker-user-spec.js
@@ -2,10 +2,17 @@ var user = require('../../lib/poker-user.js');
 
 describe('User', function() {
     it('should create a new user', function(done) {
-        user.create();
+        var userDetails = {
+            name: 'Horst Egon',
+            role: 'developer'
+        };
+
+        user.create(userDetails);
 
         user.on('user-created', function(newUser) {
             expect(newUser.id.length).toBe(40);
+            expect(newUser.name).toEqual(userDetails.name);
+            expect(newUser.role).toEqual(userDetails.role);
             done();
         });
     });
