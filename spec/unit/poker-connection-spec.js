a3e4283c1bb2bb28bbf5e9ff792fa2afd568a74f
Dominik Jungowski <Dominik@Jungowski.de>
2013-12-29 22:46:53 +0100

    spec for user removal

--- spec/unit/poker-connection-spec.js
+++ spec/unit/poker-connection-spec.js
@@ -1,5 +1,9 @@
 var pokerConnection = require('../../lib/poker-connection');
 
+// TODO: remove asap. This is ugly
+broadcastUsers = function() {};
+broadcastCards = function() {};
+
 describe('poker-connection', function() {
     it('should be initialized', function() {
         var currentUsers = [1,2,3];
@@ -44,6 +48,36 @@ describe('poker-connection', function() {
     });
 
     it('should remove the user and all listeners when connection is closed', function() {
+        var connectionMock = {
+            on: function() {},
+            removeAllListeners: function() {}
+        };
+        spyOn(connectionMock, 'on');
 
+        var usersMock = {
+            remove: function() {}
+        };
+        spyOn(usersMock, 'remove');
+
+        var carddisplayMock = {
+            removeCard: function() {}
+        };
+        spyOn(carddisplayMock, 'removeCard');
+
+        var userIdMock = 'foobar';
+
+        var connectionHandler = pokerConnection.getNewHandler();
+        connectionHandler.init(usersMock, carddisplayMock);
+        connectionHandler.setConnection(connectionMock);
+        connectionHandler.user = {
+            id: userIdMock
+        };
+
+        connectionHandler.onclose();
+
+        expect(usersMock.remove).toHaveBeenCalledWith(userIdMock);
+        expect(carddisplayMock.removeCard).toHaveBeenCalledWith(userIdMock);
+        expect(connectionMock.on).toHaveBeenCalledWith('message', jasmine.any(Function));
+        expect(connectionMock.on).toHaveBeenCalledWith('close', jasmine.any(Function));
     });
 });
