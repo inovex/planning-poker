161089816af2e2951b98ef6a0c1017d6c921eb4b
Dominik Jungowski <Dominik@Jungowski.de>
2013-12-30 12:34:38 +0100

    test for hasUser

--- spec/unit/poker-event-handlers-spec.js
+++ spec/unit/poker-event-handlers-spec.js
@@ -2,36 +2,60 @@ var pokerEventHandlers = require('../../lib/poker-event-handlers'),
     pokerBroadcaster = require('../../lib/poker-broadcaster');
 
 describe('postChatMessageListener', function() {
-    it('should set and get a user', function() {
-        var userMock = {};
-        var broadcasterMock = {};
-        var eventHandler = new pokerEventHandlers(broadcasterMock);
+    describe('#setUser/#getUser', function() {
+        it('should set and get a user', function() {
+            var userMock = {};
+            var broadcasterMock = {};
+            var eventHandler = new pokerEventHandlers(broadcasterMock);
 
-        eventHandler.setUser(userMock);
-        expect(eventHandler.getUser()).toEqual(userMock);
+            eventHandler.setUser(userMock);
+            expect(eventHandler.getUser()).toEqual(userMock);
+        });
     });
 
-    it('should broadcast a chat message', function() {
-        var broadcasterMock = {
-            broadcast: function(message) {}
-        };
-        spyOn(broadcasterMock, 'broadcast');
+    describe('#hasUser', function() {
+        it('should be false when no user is set', function() {
+            var broadcasterMock = {};
+            var eventHandler = new pokerEventHandlers(broadcasterMock);
 
-        var userMock = {};
+            expect(eventHandler.hasUser()).toBe(false);
+        });
+    });
 
-        var eventHandler = new pokerEventHandlers(broadcasterMock);
-        eventHandler.setUser(userMock);
+    describe('#postChatMessageListener', function() {
+        it('should broadcast a chat message', function() {
+            var broadcasterMock = {
+                broadcast: function(message) {}
+            };
+            spyOn(broadcasterMock, 'broadcast');
 
-        var messageData = {
-            text: 'Hallo, ich bin Bernd das Brot'
-        };
-        eventHandler.postChatMessageListener(messageData);
+            var userMock = {};
 
-        var expected = {
-            "type": 'new-chat-message',
-            "text": messageData.text,
-            "user": userMock
-        };
-        expect(broadcasterMock.broadcast).toHaveBeenCalledWith(expected);
+            var eventHandler = new pokerEventHandlers(broadcasterMock);
+            eventHandler.setUser(userMock);
+
+            var messageData = {
+                text: 'Hallo, ich bin Bernd das Brot'
+            };
+            eventHandler.postChatMessageListener(messageData);
+
+            var expected = {
+                "type": 'new-chat-message',
+                "text": messageData.text,
+                "user": userMock
+            };
+            expect(broadcasterMock.broadcast).toHaveBeenCalledWith(expected);
+        });
+
+        it('should throw an exception when trying to send a message without a user', function() {
+            var broadcasterMock = {
+                broadcast: function() {}
+            };
+            var eventHandler = new pokerEventHandlers(broadcasterMock);
+            var sendMessage = function() {
+                eventHandler.postChatMessageListener({})
+            };
+            expect(sendMessage).toThrow();
+        });
     });
 });
